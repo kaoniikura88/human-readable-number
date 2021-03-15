b6481835a307cf5e9725d83d3b2a760111efa55b
kaoniikura88 <sapozhnikova_anastasia@mail.ru>
2021-03-16 01:05:14 +0300

    init: attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    let item = {
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7': 'seven',
+        '8': 'eight',
+        '9': 'nine',
+    }
+    if (number === 0 ){
+        return 'zero'
+    }
+    let arr = number.toString().split('')
+
+
 }
